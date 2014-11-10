b7d550ad2ee3db57ac6c3d9ae5e9b1ec4df97e5f
hasan.mumin <hasan.mumin@mebitech.com>
2014-11-10 15:52:12 +0200

    ROBE-203

--- src/robe-admin-ui/app/modules/UserProfileManagement/UserProfileManagement.js
+++ src/robe-admin-ui/app/modules/UserProfileManagement/UserProfileManagement.js
@@ -1,143 +1,144 @@
 define([
-        'text!./UserProfileManagement.html',
-        'lib/cryptojs/core-min',
-        'lib/cryptojs/enc-base64-min',
-        'lib/cryptojs/sha256',
-        'kendo/kendo.grid.min',
-        'kendo/kendo.window.min',
-        'kendo/kendo.button.min',
-        'kendo/kendo.dropdownlist.min',
-        'kendo/kendo.upload.min',
-        'robe/view/RobeView'
-    ], function(view) {
+    'text!./UserProfileManagement.html',
+    'lib/cryptojs/core-min',
+    'lib/cryptojs/enc-base64-min',
+    'lib/cryptojs/sha256',
+    'kendo/kendo.grid.min',
+    'kendo/kendo.window.min',
+    'kendo/kendo.button.min',
+    'kendo/kendo.dropdownlist.min',
+    'kendo/kendo.upload.min',
+    'robe/view/RobeView'
+], function (view) {
 
-        var UserProfileManagementView = require('robe/view/RobeView').define({
-            name: "UserProfileManagementView",
-            html: view,
-            containerId: "container",
+    var UserProfileManagementView = require('robe/view/RobeView').define({
+        name: "UserProfileManagementView",
+        html: view,
+        containerId: "container",
 
-            initialize: function() {
+        initialize: function () {
 
-                var me = this;
-                $.ajax({
-                    type: "GET",
-                    url: AdminApp.getBackendURL() + "user/profile",
-                    contentType: "application/json",
-                    success: function(response) {
-                        $("#emailAddress").val(response.email);
-                        $("#firstAndLastName").val(response.name + " " + response.surname);
-                        me.data = response;
-                    },
-                    error: function(e) {
+            var me = this;
+            $.ajax({
+                type: "GET",
+                url: AdminApp.getBackendURL() + "user/profile",
+                contentType: "application/json",
+                success: function (response) {
+                    $("#emailAddress").val(response.email);
+                    $("#firstAndLastName").val(response.name + " " + response.surname);
+                    me.data = response;
+                },
+                error: function (e) {
 
-                    }
-                });
+                }
+            });
 
-                var goodColor = "#66cc66";
-                var badColor = "#ff6666";
+            var goodColor = "#66cc66";
+            var badColor = "#ff6666";
 
-                $("#newPassword").focusout(function() {
-                    if (validatePassword()) {
-                        document.getElementById('newPassword').style.background = goodColor;
-                    } else {
-                        document.getElementById('newPassword').style.background = badColor;
-                    }
-
-                });
-
-                $("#reNewPassword").keyup(function() {
-                    if (validatePassword() && isMatch()) {
-                        document.getElementById('newPassword').style.background = goodColor;
-                        document.getElementById('reNewPassword').style.background = goodColor;
-                        document.getElementById('matchMessage').innerHTML = "";
-                        document.getElementById('confirmMessage').innerHTML = "";
-                    } else {
-                        document.getElementById('newPassword').style.background = badColor;
-                        document.getElementById('reNewPassword').style.background = badColor;
-                    }
-                });
-
-
-                function validatePassword() {
-                    var error = "";
-                    var isValid = true;
-
-                    var newPassword = document.getElementById('newPassword');
-                    var message = document.getElementById('confirmMessage');
-
-                    if ((newPassword.value.length < 4) || (newPassword.value.length > 15)) {
-                        error += "Şifreniz en az 4 en fazla 15 karakter uzunluğunda olmalı.<br/>";
-                        message.innerHTML = error;
-                        isValid = false;
-                    }
-                    // Accepts Only Alphanumeric Chars
-                    if (!(newPassword.value.match(/^.*(?=.*[a-zA-Z])(?=.*\d).*$/i))) {
-                        error += "Şifrenizde en az bir adet rakam ve bir adet harf olmalıdır<br/>";
-                        message.innerHTML = error;
-
-                        isValid = false;
-                    }
-
-                    if (!(newPassword.value.match(/^\S*$/))) {
-                        error += "Şifrenizde boşluk olamaz.<br/>";
-                        message.innerHTML = error;
-
-                        isValid = false;
-                    }
-
-                    $("#confirmMessage").val(error);
-                    return isValid;
+            $("#newPassword").focusout(function () {
+                if (validatePassword()) {
+                    $("#newPassword").style.background = goodColor;
+                } else {
+                    $("#newPassword").style.background = badColor;
                 }
 
-                function isMatch() {
-                    var newPassword = document.getElementById('newPassword');
-                    var reNewPassword = document.getElementById('reNewPassword');
-                    var matchMessage = document.getElementById('matchMessage');
-                    if (newPassword.value != reNewPassword.value) {
-                        matchMessage.innerHTML = "Şifreleriniz eşleşmiyor.";
-                        return false;
-                    }
-                    return true;
+            });
+
+            $("#reNewPassword").keyup(function () {
+                if (validatePassword() && isMatch()) {
+                    $("#newPassword").style.background = goodColor;
+                    $("#reNewPassword").style.background = goodColor;
+                    $("#matchMessage").style.background = goodColor;
+                    $("#confirmMessage").style.background = goodColor;
+                } else {
+                    $("#newPassword").style.background = badColor;
+                    $("#reNewPassword").style.background = badColor;
+                }
+            });
+
+
+            function validatePassword() {
+                var error = "";
+                var isValid = true;
+
+                var newPassword = $("#newPassword");
+                var message = $("#confirmMessage");
+
+                if ((newPassword.value.length < 4) || (newPassword.value.length > 15)) {
+                    error += "Şifreniz en az 4 en fazla 15 karakter uzunluğunda olmalı.<br/>";
+                    message.innerHTML = error;
+                    isValid = false;
+                }
+                // Accepts Only Alphanumeric Chars
+                if (!(newPassword.value.match(/^.*(?=.*[a-zA-Z])(?=.*\d).*$/i))) {
+                    error += "Şifrenizde en az bir adet rakam ve bir adet harf olmalıdır<br/>";
+                    message.innerHTML = error;
+
+                    isValid = false;
                 }
 
-                $("#savePassword").kendoButton({
-                    icon: 'tick',
-                    click: function() {
+                if (!(newPassword.value.match(/^\S*$/))) {
+                    error += "Şifrenizde boşluk olamaz.<br/>";
+                    message.innerHTML = error;
 
-                        if (validatePassword() && isMatch()) {
-                            $.ajax({
-                                type: "POST",
-                                url: AdminApp.getBackendURL() + "user/updatePassword",
-                                data: {
-                                    newPassword: CryptoJS.SHA256($("#newPassword").val()).toString(),
-                                    oldPassword: CryptoJS.SHA256($("#oldPassword").val()).toString()
-                                },
-                                success: function(response) {
-                                    showToast("success", "Şifreniz Başarılı Bir Şekilde Güncellendi");
-                                    $("#oldPassword").val("");
-                                    $("#newPassword").val("");
-                                    $("#reNewPassword").val("");
+                    isValid = false;
+                }
 
-                                    document.getElementById('newPassword').style.background = "White";
-                                    document.getElementById('reNewPassword').style.background = "White";
-                                },
-                                error: function(e) {
-                                    showToast("error", "Hata: Şifre Güncellenemedi !");
-                                    $("#oldPassword").val("");
-                                    $("#newPassword").val("");
-                                    $("#reNewPassword").val("");
-
-                                    document.getElementById('newPassword').style.background = "White";
-                                    document.getElementById('reNewPassword').style.background = "White";
-                                }
-                            });
-                        } else {
-                            showToast("error", "Hata: Şifreler Uyumsuz ve Hatalı !");
-                        }
-                    }
-                });
+                $("#confirmMessage").val(error);
+                return isValid;
             }
-        });
+
+            function isMatch() {
+                var newPassword = $("#newPassword");
+                var reNewPassword = $("#reNewPassword");
+                var matchMessage = $("#matchMessage");
+                if (newPassword.value != reNewPassword.value) {
+                    matchMessage.innerHTML = "Şifreleriniz eşleşmiyor.";
+                    return false;
+                }
+                return true;
+            }
+
+            $("#savePassword").kendoButton({
+                icon: 'tick',
+                click: function () {
+
+                    if (validatePassword() && isMatch()) {
+                        $.ajax({
+                            type: "POST",
+                            url: AdminApp.getBackendURL() + "user/updatePassword",
+                            data: {
+                                newPassword: CryptoJS.SHA256($("#newPassword").val()).toString(),
+                                oldPassword: CryptoJS.SHA256($("#oldPassword").val()).toString()
+                            },
+                            success: function (response) {
+                                showToast("success", "Şifreniz Başarılı Bir Şekilde Güncellendi");
+                                $("#oldPassword").val("");
+                                $("#newPassword").val("");
+                                $("#reNewPassword").val("");
+
+
+                                $("#newPassword").style.background = "White";
+                                $("#reNewPassword").style.background = "White";
+                            },
+                            error: function (e) {
+                                showToast("error", "Hata: Şifre Güncellenemedi !");
+                                $("#oldPassword").val("");
+                                $("#newPassword").val("");
+                                $("#reNewPassword").val("");
+
+                                $("#newPassword").style.background = "White";
+                                $("#reNewPassword").style.background = "White";
+                            }
+                        });
+                    } else {
+                        showToast("error", "Hata: Şifreler Uyumsuz ve Hatalı !");
+                    }
+                }
+            });
+        }
+    });
 
     return UserProfileManagementView;
 });
