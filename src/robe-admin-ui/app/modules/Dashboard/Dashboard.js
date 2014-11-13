b2936f6b7bf67b94b381290f4bb3cb2921d4ba02
Seray Uzgur <seray.uzgur@mebitech.com>
2014-11-13 19:31:30 +0200

    ROBE-211 Dashboard with bootstrap

--- src/robe-admin-ui/app/modules/Dashboard/Dashboard.js
+++ src/robe-admin-ui/app/modules/Dashboard/Dashboard.js
@@ -1,15 +1,11 @@
-var Dashboard;
-
 define([
     'text!./Dashboard.html',
+    'robe/view/metrics/Grid',
+    'robe/view/metrics/ProgressBarGroup',
     'kendo/kendo.button.min',
     'kendo/kendo.dropdownlist.min',
-    'lib/highcharts/highcharts',
-    'lib/highcharts/highcharts-more',
-    'lib/highcharts/exporting',
-    'robe/Charts',
     'robe/view/RobeView'
-], function(view) {
+], function(view,Grid,ProgressBarGroup) {
 
     var Dashboard = require('robe/view/RobeView').define({
         name: "Dashboard",
@@ -23,112 +19,123 @@ define([
                     return false;
                 }
             });
+            this.downloadMetricData();
+        },
+
+        downloadMetricData: function () {
+            var me = this;
             $.ajax({
-                type: "GET",
-                url: AdminApp.getBackendURL() + "../admin/metrics",
-                dataType: "json",
-                crossDomain: true,
-
-                contentType: "application/json; charset=utf-8",
-                success: function(response) {
-                    var data = response["gauges"];
-                    var servletInfo = response["meters"];
-                    var serverUptime = "Server Uptime : " + response["gauges"]["jvm.gc.PS-Scavenge.time"]["value"] + " seconds";
-
-
-                    //Memory data
-                    var memoryData = [
-                        ["Unused", (data["jvm.memory.total.max"]["value"] - data["jvm.memory.total.used"]["value"])],
-                        ["Used", (data["jvm.memory.total.used"]["value"])]
-                    ];
-
-                    //Connection Pool Data
-
-                    var connMax = data["jvm.threads.count"].value + data["jvm.threads.daemon.count"].value;
-                    var connUsed = data["jvm.threads.runnable.count"].value;
-
-
-                    //Logback Data
-                    var logbackAll = response["meters"]["ch.qos.logback.core.Appender.all"]["count"];
-                    var logbackDebug = response["meters"]["ch.qos.logback.core.Appender.debug"]["count"];
-                    var logbackError = response["meters"]["ch.qos.logback.core.Appender.error"]["count"];
-                    var logbackInfo = response["meters"]["ch.qos.logback.core.Appender.info"]["count"];
-                    var logbackTrace = response["meters"]["ch.qos.logback.core.Appender.trace"]["count"];
-                    var logbackWarn = response["meters"]["ch.qos.logback.core.Appender.warn"]["count"];
-                    var logBackData = [
-                        ["Debug", (logbackDebug)],
-                        ["Error", (logbackError)],
-                        ["Info", (logbackInfo)],
-                        ["Trace", (logbackTrace)],
-                        ["Warn", (logbackWarn)]
-                    ];
-
-
-                    //Heap Memory Data
-                    var heapMemoryData = [
-                        ["Unused", (data["jvm.memory.heap.max"]["value"] - data["jvm.memory.heap.used"]["value"])],
-                        ["Used", (data["jvm.memory.heap.used"]["value"])]
-                    ];
-                    var heapUsage = data["jvm.memory.heap.usage"]["value"];
-                    var nonHeapUsage = data["jvm.memory.non-heap.usage"]["value"];
-
-                    var vmName = "";
-                    var vmVersion = response["version"];
-                    var httpResponseCounts = [servletInfo["io.dropwizard.jetty.MutableServletContextHandler.1xx-responses"]["count"], servletInfo["io.dropwizard.jetty.MutableServletContextHandler.2xx-responses"]["count"], servletInfo["io.dropwizard.jetty.MutableServletContextHandler.3xx-responses"]["count"], servletInfo["io.dropwizard.jetty.MutableServletContextHandler.4xx-responses"]["count"], servletInfo["io.dropwizard.jetty.MutableServletContextHandler.5xx-responses"]["count"]];
-                    var categoryList = ['1**', '2**', '3**', '4**', '5**'];
-
-                    var timers = response["timers"];
-
-                    var resourcesNames = [];
-                    var resourcesDataCount = [];
-                    var resourcesDataMean = [];
-                    var resourcesDataMax = [];
-                    var resourcesDataMin = [];
-                    for (var key in timers) {
-                        if (timers.hasOwnProperty(key))
-                            if (key.match("^io.robe.admin.resources")) {
-                                var names = key.split('.');
-                                resourcesNames.push(names[names.length - 1]);
-                                resourcesDataCount.push(timers[key]["count"]);
-                                resourcesDataMean.push(timers[key]["mean"]);
-                                resourcesDataMax.push(timers[key]["max"]);
-                                resourcesDataMin.push(timers[key]["min"]);
-                            }
-                    }
-                    var seriesTime = [{
-                        name: 'Mean',
-                        data: resourcesDataMean
-                    }, {
-                        name: 'Max',
-                        data: resourcesDataMax
-                    }, {
-                        name: 'Min',
-                        data: resourcesDataMin
-                    }];
-                    var seriesCount = [{
-                        name: 'Count',
-                        data: resourcesDataCount
-                    }];
-
-                    Charts.pie("memory", memoryData, "Ram");
-                    Charts.gauge("threads", [connUsed, connMax], "Threads");
-                    Charts.pie("allLogback", logBackData, "LOGBACK");
-                    Charts.pie("heapMemory", heapMemoryData, "Heap Memory");
-                    Charts.column("responseCount", httpResponseCounts, 'Http Response Code', " Response Counts", "HTTP Response Code Counts", serverUptime, categoryList);
-                    Charts.row("robeAdminResourcesCount", resourcesNames, seriesCount, "Count");
-                    Charts.row("robeAdminResourcesTime", resourcesNames, seriesTime, "Second");
-                    document.getElementById("vmName").innerHTML = vmName;
-                    document.getElementById("vmVersion").innerHTML = vmVersion;
-                    document.getElementById("heapUsage").innerHTML = "%" + parseInt(heapUsage * 100);
-                    document.getElementById("nonHeapUsage").innerHTML = "%" + parseInt(nonHeapUsage * 100);
-
+                contentType : "application/json",
+                url : AdminApp.getBackendURL() + "../admin/metrics",
+                success : function(data) {
+                    me.parseMetricsJson(data);
                 },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    alert('HTTP Error: ' + errorThrown + ' | Error Message: ' + textStatus);
-                    return;
-                }
+                error : function(xhr, ajaxOptions, thrownError) {
+                    //alert("Error - " + xhr.status + "Message: " + thrownError);
+                    metricsWatcher.initGraphs();
+                },
+                async : false
             });
+        },
+
+        parseMetricsJson: function(json){
+            var meters = json['meters'];
+            // RESPONSE
+            var totalRequests = [
+                "2xx",meters['io.dropwizard.jetty.MutableServletContextHandler.2xx-responses'],
+                "3xx",meters['io.dropwizard.jetty.MutableServletContextHandler.3xx-responses'],
+                "4xx",meters['io.dropwizard.jetty.MutableServletContextHandler.4xx-responses'],
+                "5xx",meters['io.dropwizard.jetty.MutableServletContextHandler.5xx-responses']
+            ];
+            var metricList = [
+                '1 min','m1_rate',
+                '5 min','m5_rate',
+                '15 min','m15_rate',
+                'Mean','mean_rate'
+            ];
+            ProgressBarGroup.define().render('totalReq', 'Total', totalRequests,'count',1);
+            var MutableServletContextHandler200 = meters['io.dropwizard.jetty.MutableServletContextHandler.2xx-responses'];
+            Grid.define().render('2xxReq', '2xx-response (events/second)',metricList, MutableServletContextHandler200);
+            var MutableServletContextHandler400 = meters['io.dropwizard.jetty.MutableServletContextHandler.4xx-responses'];
+            Grid.define().render('4xxReq', '4xx-response (events/second)',metricList, MutableServletContextHandler400);
+            var MutableServletContextHandler500 = meters['io.dropwizard.jetty.MutableServletContextHandler.5xx-responses'];
+            Grid.define().render('5xxReq', '5xx-response (events/second)',metricList, MutableServletContextHandler500);
+
+
+
+            //LOGS
+            var appenderData = [
+                "Debug",meters['ch.qos.logback.core.Appender.debug'],
+                "Info",meters['ch.qos.logback.core.Appender.info'],
+                "Warn",meters['ch.qos.logback.core.Appender.warn'],
+                "Error",meters['ch.qos.logback.core.Appender.error']
+            ];
+            ProgressBarGroup.define().render('logbackLogs', 'Logback', appenderData,'count',1);
+
+
+            var gauges = json['gauges'];
+            //VM
+
+            var unusedTotal = gauges["jvm.memory.total.max"]['value'] - gauges["jvm.memory.total.used"]['value'];
+            var totalData = [
+                "Used",gauges["jvm.memory.total.used"],
+                "Free",{'value':unusedTotal}
+            ];
+            ProgressBarGroup.define().render('totalMem', 'Total (MB)', totalData,'value',1024*1024);
+
+            var unusedHeap = gauges["jvm.memory.heap.max"]['value'] - gauges["jvm.memory.heap.used"]['value'];
+            var heapData = [
+                "Used",gauges["jvm.memory.heap.used"],
+                "Free",{'value':unusedHeap}
+            ];
+            ProgressBarGroup.define().render('heapMem', 'Heap (MB)', heapData,'value',1024*1024);
+
+            var unusednonHeap = gauges["jvm.memory.non-heap.max"]['value'] - gauges["jvm.memory.non-heap.used"]['value'];
+            var nonheapData = [
+                "Used",gauges["jvm.memory.non-heap.used"],
+                "Free",{'value':unusednonHeap}
+            ];
+            ProgressBarGroup.define().render('nonheapMem', 'Non-Heap (MB)', nonheapData,'value',1024*1024);
+
+            var poolData = [
+                "Eden",gauges["jvm.memory.pools.PS-Eden-Space.usage"],
+                "Old",gauges["jvm.memory.pools.PS-Old-Gen.usage"],
+                "Perm",gauges["jvm.memory.pools.PS-Perm-Gen.usage"],
+                "Survior",gauges["jvm.memory.pools.PS-Survivor-Space.usage"]
+            ];
+            ProgressBarGroup.define().render('poolMem', 'Pool Status (%)', poolData,'value',0.01,100);
+
+            var jvm = gauges;
+            metricList = [
+                "Runnable","jvm.threads.runnable.count",
+                "New","jvm.threads.new.count",
+                "Timed-W","jvm.threads.timed_waiting.count",
+                "Waiting","jvm.threads.waiting.count",
+                "Blocked","jvm.threads.blocked.count",
+                "Terminated","jvm.threads.terminated.count"
+            ];
+            jvm.count = jvm["jvm.threads.count"]['value'];
+
+            Grid.define().render('threads', 'Threads (count)',metricList, jvm,"value",0);
+
+
+
+            // SERVICES
+            metricList = [
+                "Max", "max",
+                "Min", "min",
+                "Mean", "mean"];
+            var serviceList = ["io.robe.auth.tokenbased.TokenBasedAuthenticator.gets","io.robe.admin.resources.AuthResource.login"];
+            var serviceNameList = ["TokenBasedAuthenticator.gets","AuthResource.login"];
+
+            var timers = json['timers'];
+            for(var i = 0 ; i < serviceList.length; i++){
+                var data = timers[serviceList[i]];
+                Grid.define().render('service' + (i+1), serviceNameList[i] + ' (Sec)',metricList, data);
+            }
+
         }
+
     });
 
     return Dashboard;
