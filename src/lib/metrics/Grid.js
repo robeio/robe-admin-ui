c16b2211ff417e7f7f02f639ee87e58714baf142
Seray Uzgur <seray.uzgur@mebitech.com>
2015-08-04 16:53:52 +0300

    Merge branch 'master' of /Users/serayuzgur/Development/robe-github/robe-admin-ui with conflicts.

---
+++ src/lib/metrics/Grid.js
@@ -0,0 +1,29 @@
+define([], function() {
+    var Grid = new Object();
+    Grid.define = function() {
+        var me = new Object();
+
+        me.render = function(containerId, title,metricList, model,valueKey,scale) {
+            var header = "<div class='panel panel-default'><div class='panel-heading'><div class='panel-title'>" + title + "</div>"+model.count+" total</div><div class='panel-body container '>";
+            scale = ((scale === undefined) ? 4 : scale );
+            for(var i = 0 ; i < metricList.length ; i++){
+                if(valueKey){
+                    header += this.createMeterRow(metricList[i++],model[metricList[i]][valueKey].toFixed(scale));
+                }else{
+                    header += this.createMeterRow(metricList[i++],model[metricList[i]].toFixed(scale));
+                }
+            }
+
+            header += "</div></div>";
+            $('#' + containerId).html(header);
+
+        };
+        me.createMeterRow = function(label,value) {
+
+            return "<div class='row '> <div class='col-md-1 column text-right'>" + label + ":</div><div class='col-md-1 column'>" + value + "</div> </div>";
+        }
+        return me;
+    };
+
+    return Grid;
+});
