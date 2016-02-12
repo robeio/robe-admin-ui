3b65d0d62d3b6f0b3ffdafb3444b4e95d95645a7
hasanmumin <hasan.mumin@mebitech.com>
2016-02-12 14:18:42 +0200

    fixed internet explorer cache problem.

--- src/app/common/SingletonDataSource.js
+++ src/app/common/SingletonDataSource.js
@@ -1,19 +1,43 @@
 define([
     'robe/core/Singleton',
     'kendo/kendo.data.min'
-], function(Singleton,DataSource) {
+], function (Singleton, DataSource) {
     var SingletonDataSource = Singleton.define({
-        initialize: function() {
+        initialize: function () {
+
+            var transport = this.parameters.transport;
+
+            /**
+             * TODO setting default cache false for internet explorer cache problem.
+             */
+
+            if (transport.read) {
+                transport.read.cache = false;
+            }
+
+            if (transport.destroy) {
+                transport.destroy.cache = false;
+            }
+
+            if (transport.update) {
+                transport.update.cache = false;
+            }
+
+            if (transport.create) {
+                transport.create.cache = false;
+            }
+
             this.data = new kendo.data.DataSource(this.parameters);
             this.data.bind("error", this.requestError);
             this.data.bind("requestEnd", this.requestEnd);
+            this.data.bind("requestStart", this.requestStart);
         },
 
-        read: function() {
+        read: function () {
             this.data.read();
         },
 
-        requestError: function(e) {
+        requestError: function (e) {
             var response = e.response;
             var type = e.type;
             var message = "";
@@ -30,19 +54,27 @@ define([
                 showToast("error", message);
         },
 
-        requestEnd: function(e) {
+        requestEnd: function (e) {
+            kendo.ui.progress($("#body"), false);
             var response = e.response;
             var type = e.type;
             var message = "";
             if (type === "update") {
                 message = "Başarı ile güncellendi";
+                this.read();
             } else if (type === "destroy") {
                 message = "Başarı ile silindi";
+                this.read();
             } else if (type === "create") {
                 message = "Başarı ile oluşturuldu";
+                this.read();
             }
             if (message != "")
                 showToast("success", message);
+        },
+
+        requestStart: function (e) {
+            kendo.ui.progress($("#body"), true);
         }
     });
     return SingletonDataSource;
